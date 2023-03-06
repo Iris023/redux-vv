9cc613095ce9e76b2a2c4741afe43f9a2a07d9e8
Iris🌸 <Iriska@IrisMacBook.local>
2023-03-06 14:54:45 +0300

    commit

--- src/Components/FilterComponent/AllCategories.js
+++ src/Components/FilterComponent/AllCategories.js
@@ -3,7 +3,7 @@ import { Filter } from "./Filter"
 export const AllCategories = () => {
     return(<div className="allTheCategories">
         {['ВСЕ', 'ФРУКТЫ', 'СЛАДОСТИ', 'МОЛОЧНЫЕ ПРОДУКТЫ', 'ХЛЕБ']
-        .map((category, id) =>  <Filter key={id} category={category}/>)
+        .map((category, index) =>  <Filter key={index} category={category}/>)
         }
     </div>)
 }
